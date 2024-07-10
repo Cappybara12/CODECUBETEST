fc3a83f35064b98d0dda260e5070ae6576a688e9
cappybara12 <akshayne912@gmail.com>
2024-07-11 04:13:46 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import viteSvgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/CODECUBETEST/",
   plugins: [
     react(),
     viteSvgr({
