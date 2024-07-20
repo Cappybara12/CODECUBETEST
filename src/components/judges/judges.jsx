4e76c5d10b74dbdb770129158c3e8a3eedc17c17
Aditya <adityakumarji74@gmail.com>
2024-07-20 13:17:16 +0530

    about us background fixed and mentors added

--- src/components/judges/judges.jsx
+++ src/components/judges/judges.jsx
@@ -1,37 +1,54 @@
 import React, { useEffect } from "react";
 import "./judges.css";
 import Judge3 from "./Judge3";
-import { set1, set2, set3, set4 } from "./Details";
+import {
+  set1,
+  set2,
+  set3,
+  set4,
+  set5,
+  set6,
+  set7,
+  set8,
+  set9,
+  set10,
+  set11,
+  set12,
+  set13,
+} from "./Details";
 const judges = () => {
   useEffect(() => {
     document.title = "Judge Panel | Hack a Tank";
-  },[]);
-
+  }, []);
 
   return (
     <div className="parent-judges">
       <section className="bg-judges">
-        
-            <img src="/images/MentorsBG.png" alt="hjhg" className="bg_img" />
-        
-        <div className="bg_img-overlay"></div>
+        <img src="/images/MentorsBG.png" alt="hjhg" className="bg_img" />
 
+        <div className="bg_img-overlay"></div>
       </section>
       <div className="title">
         <h1>mentor panel</h1>
       </div>
       <section className="judges">
-        
-      <Judge3 mem = {set1} />
-      <Judge3 mem = {set2} />
-      <Judge3 mem = {set3} />
-      <Judge3 mem = {set4} />
-      
-      {/* <h1 className="title">COMING SOON!</h1> */}
+        <Judge3 mem={set1} />
+        <Judge3 mem={set2} />
+        <Judge3 mem={set3} />
+        <Judge3 mem={set4} />
+        <Judge3 mem={set5} />
+        <Judge3 mem={set6} />
+        <Judge3 mem={set7} />
+        <Judge3 mem={set8} />
+        <Judge3 mem={set9} />
+        <Judge3 mem={set10} />
+        <Judge3 mem={set11} />
+        <Judge3 mem={set12} />
+        <Judge3 mem={set13} />
+
+        {/* <h1 className="title">COMING SOON!</h1> */}
       </section>
-      
     </div>
-    
   );
 };
 
